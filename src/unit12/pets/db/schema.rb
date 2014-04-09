365f08c5aacc48aea7c36342486e602c0b193f98
Derek Schissler <dschissl@mines.edu>
2014-04-08 21:35:07 -0600

    added is_admin field to users table and created admin user seed

--- src/unit12/pets/db/schema.rb
+++ src/unit12/pets/db/schema.rb
@@ -57,10 +57,15 @@ ActiveRecord::Schema.define(version: 20140408232138) do
     t.string   "status"
   end
 
-  create_table "user_sessions", force: true do |t|
+  create_table "sessions", force: true do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at"
 
   create_table "users", force: true do |t|
     t.string   "username"
@@ -68,6 +73,7 @@ ActiveRecord::Schema.define(version: 20140408232138) do
     t.string   "crypted_password"
     t.string   "password_salt"
     t.string   "persistence_token"
+    t.boolean  "is_admin"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
--- src/unit12/pets/db/seeds.rb
+++ src/unit12/pets/db/seeds.rb
@@ -9,4 +9,4 @@
 Pet.create(name: 'Sparky', age: 5, pet_type: 'dog', description: 'A fun, loving puppy', image_url: 'sparky.jpg', status: 'Avaliable')
 Pet.create(name: 'Iggy', age: 2.5, pet_type: 'iguana', description: 'He\'s green; He\'s scaly; He\'s lovable.', image_url: 'iggy.jpg', status: 'Avaliable')
 Pet.create(name: 'Jack', age: 1, pet_type: 'kitten', description: 'Jack is the cutest kitten ever!', image_url: 'jack.jpg', status: 'Avaliable')
-
+User.create(username: 'admin', email:'admin@minespetadoption.com', is_admin: true, password:'password', password_confirmation:'password')
