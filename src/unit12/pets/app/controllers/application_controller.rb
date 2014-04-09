d82e203dd0a701601264ed52ead8b48d62793d6e
Derek Schissler <dschissl@mines.edu>
2014-04-08 18:01:55 -0600

    improved login display

--- src/unit12/pets/app/controllers/application_controller.rb
+++ src/unit12/pets/app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :current_user
+
   before_action :set_current_consideration, only: [:index, :show, :edit, :update, :destroy]
 
   private
@@ -25,5 +27,14 @@ class ApplicationController < ActionController::Base
       consideration = Consideration.create
       session[:consideration_id] = consideration.id
       consideration
+    end
+
+    def current_user_session  
+      return @current_user_session if defined?(@current_user_session)  
+      @current_user_session = UserSession.find  
+    end  
+      
+    def current_user  
+      @current_user = current_user_session && current_user_session.record  
     end
 end
