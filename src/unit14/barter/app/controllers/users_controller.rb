52e3d32f3545b03ac7689ccd695ec9acbbc90c0e
Derek Schissler <dschissl@mines.edu>
2014-04-22 22:29:14 -0600

    added ability to create a new user

--- src/unit14/barter/app/controllers/users_controller.rb
+++ src/unit14/barter/app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
-  def edit
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to animals_url, notice: 'Registration successful.' }
+        format.json { render action: 'show', status: :created, location: @user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
 end
